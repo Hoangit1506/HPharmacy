0ca1de6d5b9d33486bff2c87796dfa2b4b3eeda7
Hoangit1506 <hoangit1506@gmail.com>
2024-10-23 20:14:13 +0700

    Create - add properties

---
+++ src/test/java/com/project/HPharmacy/HPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.HPharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HPharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
